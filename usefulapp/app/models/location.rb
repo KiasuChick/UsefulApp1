aff0bad0ec8c6e1b3d1264000eccaccc10e64e11
KiasuChick <me@kiasuchick.com>
2014-05-18 14:40:21 -0700

    added validates statements

--- usefulapp/app/models/location.rb
+++ usefulapp/app/models/location.rb
@@ -3,4 +3,9 @@ class Location
   field :name, type: String
   field :address, type: String
   field :store_type, type: String
+
+  validates :name, presence: :true
+  validates :address, presence: :true
+  validates :store_type, presence: :true
+
 end
